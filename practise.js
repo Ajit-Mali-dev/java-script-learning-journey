966b0cb15f2af31bbb9728da43171668964364c9
Ajit-Mali-dev <ajitmali1964@gmail.com>
2025-09-09 17:11:06 +0530

    String and its methods

---
+++ practise.js
@@ -0,0 +1,18 @@
+
+let userName = "Ajit"
+let lastName ="Mali"
+// console.log(`My first name is ${userName} and last name is  ${lastName}`);
+
+// console.log(typeof userName);
+
+// console.log(userName.length);
+// console.log(userName.indexOf('t'));
+
+// console.log(userName.replace("i", "ee"));
+
+// console.log(userName)
+
+// console.log(userName.charAt(3));
+// console.log(userName.indexOf("t"));
+
+l
