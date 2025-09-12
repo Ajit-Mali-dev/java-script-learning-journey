8da15ea6e1a67f17db384310402d72f511e602db
Ajit-Mali-dev <ajitmali1964@gmail.com>
2025-09-12 18:10:20 +0530

    Understanding basics of array

---
+++ arrays.js
@@ -0,0 +1,22 @@
+let Array =  [1, 2, 3, 4, 5]
+
+
+const newArr = Array.join()  // Turn any Array into a String  .
+
+// console.log(Array);   // Datatype is Array
+// console.log( newArr); // Datatype turned into string
+
+
+// slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3) // excludes the ending condition and doesn't have affect on original array
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3) // includes the ending condition and change the original array
+// console.log("C ", myArr);
+// console.log(myn2);
